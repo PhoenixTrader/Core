77a58be98302f6484a28fad9134c61a580325f5b
ErikMann <41575920+ErikMann@users.noreply.github.com>
2022-01-09 15:19:06 +0100

    add_IB_third_party_class

---
+++ Core/src/main/java/IB_connect/apidemo/OperatorConditionPanel.java
@@ -0,0 +1,30 @@
+/* Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+package IB_connect.apidemo;
+
+import IB_connect.apidemo.util.TCombo;
+import IB_connect.apidemo.util.UpperField;
+import IB_connect.ib.client.OperatorCondition;
+
+public class  OperatorConditionPanel<T extends OperatorCondition> extends OnOKPanel {
+	final T m_condition;
+	final TCombo<String> m_operator = new TCombo<>("<=", ">=");
+	final UpperField m_value = new UpperField();
+	
+	OperatorConditionPanel(T condition) {
+		m_condition = condition;
+		
+		m_operator.setSelectedIndex(m_condition.isMore() ? 1 : 0);
+	}
+	
+	public T onOK() {
+		m_condition.isMore(m_operator.getSelectedIndex() == 1);
+		
+		return m_condition;
+	}
+	
+	protected T condition() {
+		return m_condition;
+	}
+}
